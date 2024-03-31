f997ec45951d082bc0d42e73b6f113961ab6fec7
Chrysille <chrysille_so@dlsu.edu.ph>
2024-03-31 19:00:20 +0800

    Fixed asmfunc computation

--- x86_64_GuillarteSo/asmfunc.asm
+++ x86_64_GuillarteSo/asmfunc.asm
@@ -1,29 +1,48 @@
+%include "io64.inc" ;remove when calling from C
+
+;sample data for test, remove when calling from C
+section .data
+vector1 dq 1.0, 2.0, 3.0
+vector2 dq 1.0, 2.0, 3.0
+size dd 3
+
+
 section .text
 bits 64
 default rel
 global calculateDotProduct
-extern printf
+
 
 calculateDotProduct:
+    
+    ;temp move, remove when calling from C
+    lea rdi, [vector1]
+    lea rsi, [vector2]
+    mov rdx, [size]
+   
+    ;counter
+    mov rbx, 0
     push rbp
     mov rbp, rsp
     add rbp, 16
     xorpd xmm0, xmm0             ; Clear xmm0 (dotProduct)
+    mov r8, rdx                   ; Load size to R8
     mov rcx, rdi                  ; Load vector1 pointer to RCX
     mov rdx, rsi                  ; Load vector2 pointer to RDX
-    mov r8, rdx                   ; Load size to R8
-    shl r8, 3                     ; Multiply size by 8 (sizeof(double))
-    add r8, rcx                   ; Calculate end address of vector1
+    
+
 loop_start:
-    cmp rcx, r8                   ; Compare current address with end address
+    cmp rbx, r8                   ; Compare current address with end address
     je loop_end                   ; If equal, exit loop
     movsd xmm1, QWORD [rcx]      ; Load double from vector1 to xmm1
     movsd xmm2, QWORD [rdx]      ; Load double from vector2 to xmm2
     mulsd xmm1, xmm2              ; Multiply vector1 element with vector2 element
     addsd xmm0, xmm1              ; Add the result to dotProduct
     add rcx, 8                   ; Move to next element in vector1
-    add rdx, 8                   ; Move to next element in vector2
+    add rdx, 8    
+    INC rbx               ; Move to next element in vector2
     jmp loop_start                ; Repeat the loop
 loop_end:
     pop rbp
+    xor rax, rax
     ret
